d76e8c07789dcb801a085f528d717c0c2822275c
Henry Nawrocki <hdev@sztanski.com>
2022-07-16 16:38:09 -0500

    Add custom color button 11:25

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import { Button } from '@mui/material';
+import { Settings, Add } from '@mui/icons-material';
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Button variant="text">Text</Button>
+      <Button
+        startIcon={<Settings />}
+        variant="contained"
+        color="secondary"
+        size="large"
+      >
+        SETTINGS
+      </Button>
+      <Button
+        startIcon={<Add />}
+        variant="contained"
+        color="success"
+        size="large"
+      >
+        ADD NEW POST
+      </Button>
+      <Button variant="outlined">Outlined</Button>
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: 'skyblue',
+          color: '#888',
+          margin: 5,
+          '&:hover': {
+            backgroundColor: 'lightblue',
+          },
+        }}
+      >
+        My unique button
+      </Button>
     </div>
   );
 }
